4db134466ddc5a2281fdfb12c9c45a1045acbadf
cheng <50474794+chengleee@users.noreply.github.com>
2025-05-20 19:56:59 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -78,10 +78,10 @@ const mainStage = new Stage("main-canvas");
 const stages = [trailsStage, mainStage];
 
 //随机文字烟花内容
-const randomWords = ["新年快乐", "心想事成"];
+const randomWords = ["熊熊 520 快乐", "枨枨 love 熊熊"];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
-	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "90px");
+	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "100px");
 });
 
 // 自定义背景
